42269281cac70defadff3bbb86aa3fd84e6f616e
umutcangur <35804675+umutcangur@users.noreply.github.com>
2025-04-07 14:51:44 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,58 @@
+Servers = [1323692659486752880] #The actual server/guild ids
+Channels = {1332686965484552253: []}
+Token = 'MTMzMzQ0MzQ0NjA1MTk2Mjg5.GWtOmQ.zhx57dgnEAWehQoOZ5fcz_30Eq9F8xrq47IUNo'
+Rollcommand = 'ha'
+Rollprefix = '_'
+
+AlwaysRoll = False #Whether it continue to roll after a claim or not
+Pokeroll = False # Whether you want to pokeroll or not i guess
+Snipe = True #True to enable sniping others
+SnipeKak = True #True to enable sniping others
+Delay = 40 #Time for delaying claim
+DelaySelf = 0 #Time for delaying self claim
+DelayKak = 30 #Time for delaying kakera
+DelayKakSelf= 0 #Time for delaying self kakera
+AutoRoll = True
+AutoDk = False
+Message = None #message you want to send when you claim a character, make it None to send nothing
+# Example: Message = None (not sure if Message = '' will work let me know !!!)
+
+Wishlist = [
+'Monkey D. Luffy',
+'Shiro',
+'Yuji Itadori',
+'Eren Jaeger',
+'Megumi Fushiguro',
+'Tanjirou Kamado',
+'Suguru Geto',
+'Guts',
+'Jotaro Kujo',
+'Kyoujurou Rengoku',
+'Marceline',
+'Eris Boreas Greyrat',
+'Mine',
+'Utahime Iori',
+'Melina'
+            ]
+
+#Durations in minutes 
+Daily = 1200
+Claim = 180
+Roll = 60
+
+#Any Kakera in this List will be IGNORED AND NOT SNIPED, remove the ones you WANT TO CLAIM for the bot to claim them. 
+Kakera = ['Kakera','KakeraT','KakeraB','KakeraG']
+# All Known Kakera emojis
+# 'KakeraP',
+# 'Kakera',
+# 'KakeraT',
+# 'KakeraG',
+# 'KakeraY',
+# 'KakeraO',
+# 'KakeraR',
+# 'KakeraW',
+# 'KakeraL',
+
+#claim stuff
+minkak = 250 #minimum kakera to claim
+lastminkak = 150 #minimum kakera to claim in last 60 minutes before reset
